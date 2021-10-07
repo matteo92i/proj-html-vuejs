8d432265e413dfa534030052ca424f21a64cd310
Matteo92i <matteo.iovane@gmail.com>
2021-10-07 15:21:56 +0200

    fine parte 2

--- src/components/MainPart2.vue
+++ src/components/MainPart2.vue
@@ -1,5 +1,33 @@
 <template>
-    <div></div>
+    <div>
+        <div class="container-fluid">
+            <div class="row mt-5">
+                <div class="col-4">
+                    <img class="img-fluid center-sushi" src="../assets/img/sushi-1.png" alt="sushi-1">
+                    <h2>the best table in town</h2>
+                    <hr class="center-sushi">
+                    <p class="center-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, modi est, ullam natus sunt id reiciendis ex cumque accusamus provident adipisci!</p>
+                    <button class="center-sushi stile-1">explore the menu</button>
+                </div>
+
+                <div class="col-4">
+                    <img class="img-fluid center-sushi" src="../assets/img/sushi-2.png" alt="sushi-2">
+                    <h2>the best table in town</h2>
+                    <hr class="center-sushi">
+                    <p class="center-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, modi est, ullam natus sunt id reiciendis ex cumque accusamus provident adipisci!</p>
+                    <button class="center-sushi stile-1">explore the menu</button>
+                </div>
+
+                <div class="col-4">
+                    <img class="img-fluid center-sushi" src="../assets/img/sushi-3.png" alt="sushi-3">
+                    <h2>the best table in town</h2>
+                    <hr class="center-sushi">
+                    <p class="center-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, modi est, ullam natus sunt id reiciendis ex cumque accusamus provident adipisci!</p>
+                    <button class="center-sushi stile-1">explore the menu</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +37,32 @@ export default {
 </script>
 
 <style>
+h2{
+    text-transform:uppercase;
+    font-size: 15px;
+    padding-top: 15px;
+    text-align: center;
+}
+hr{
+    width: 90px;
+}
+.center-1{
+    text-align: center;
+}
+.center-sushi {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.stile-1{
+    border: 1px solid black;
+    background-color: white;
+    text-transform:uppercase;
+}
+    
+  
+    
+
+
 
 </style>
