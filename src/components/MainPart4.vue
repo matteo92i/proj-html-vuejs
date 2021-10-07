38ac9fe75d098a224accabf3988f2d4cfd6b5272
Matteo92i <matteo.iovane@gmail.com>
2021-10-07 16:31:37 +0200

    fine 4 parte

--- src/components/MainPart4.vue
+++ src/components/MainPart4.vue
@@ -1,5 +1,32 @@
 <template>
-    <div></div>
+    <div>
+        <div class="container">
+            <div class="row mt-5">
+                <h1 class="font-italic text-center mb-5">what critics are saying about us</h1>
+
+                <div class="col-6 p-0">
+                    <img class="img-sfondo" src="../assets/img/blog72x.jpg" alt="blog72">
+                </div>
+
+                <div class="col-6 sfondo p-0">
+
+                    <div class="margini">
+                        <h5>meggy stewart</h5>
+                        <h1 class="h1-white">new york times</h1>
+                        <hr class="hr-white">
+                        <p class="p-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet incidunt fuga cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="p-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit molestias aspernatur animi.</p>
+                        <button class="stile">read full article</button>
+                    </div>
+                    
+                </div>
+                <div class="col-6"></div>
+                <div class="col-6">
+                    <img src="../assets/img/info12x.jpg" alt="info12x">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +36,17 @@ export default {
 </script>
 
 <style lang="scss">
+.hr-white{
+    color: white;
+    width: 90px;
+}
+.margini{
+    margin-left:120px ;
+    margin-top: 150px;
+    margin-right: 90px;
+}
+
+
+
 
 </style>
