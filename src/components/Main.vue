5e02b2635482990ca46261ad0f487ce9ea9f8ace
Matteo92i <matteo.iovane@gmail.com>
2021-10-07 23:16:14 +0200

    fine Parte 5

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,6 +4,7 @@
         <MainPart2 />
         <MainPart3 />
         <MainPart4 />
+        <MainPart5 />
 
     </div>
 </template>
@@ -13,6 +14,7 @@ import MainPart1 from "./MainPart1.vue";
 import MainPart2 from "./MainPart2.vue";
 import MainPart3 from "./MainPart3.vue";
 import MainPart4 from "./MainPart4.vue";
+import MainPart5 from "./MainPart5.vue";
 
 export default {
 name: 'Main',
@@ -21,6 +23,7 @@ components: {
     MainPart2,
     MainPart3,
     MainPart4,
+    MainPart5,
 }
 
     
---
+++ src/components/MainPart5.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 banner mt-5">
+                    <h5 class="text-center">Kung Pang -- 32£</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+.banner{
+    background-image: url(../assets/img/page52x.jpg);
+    background-color: #cccccc; 
+    height: 400px; 
+    background-position: center; 
+    background-repeat: no-repeat; 
+    background-size: cover;
+    width: 100%;
+}
+    
+    
+
+</style>
