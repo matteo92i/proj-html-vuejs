7c0cf5e8279cbba472cc55f8f130c19c35cd0998
Matteo92i <matteo.iovane@gmail.com>
2021-10-07 15:50:45 +0200

    fine parte 3

--- src/components/MainPart3.vue
+++ src/components/MainPart3.vue
@@ -2,10 +2,32 @@
     <div>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-7"></div>
+                <div class="col-7 p-0">
+                    <img class="img-sfondo" src="../assets/img/slider32x.jpg" alt="slider">
+                </div>
 
-                <div class="col-5 sfondo"></div>
+                <div class="col-5 sfondo p-0">
+                    <div class="col-8 mx-auto mt-5 p-3">
+                      <h5> fine dining experience </h5>
+                    </div>
+                        
+
+                    <div class="col-8 mx-auto p-3">
+                      <h1 class="h1-white">the best table in town  </h1>
+                    </div>
+
+                    <div class="col-8 mx-auto p-3">
+                      <p class="p-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ratione assumenda ad corrupti atque architecto ex impedit aperiam? Perspiciatis cum in unde recusandae blanditiis saepe alias maxime molestias aperiam dolorum!</p>
+                    </div>
+
+                    <div class="col-8 mx-auto p-3">
+                      <button class="stile">explore the menu</button>
+                    </div>
+
+                </div>
+
             </div>
+
         </div>
     </div>
 </template>
@@ -17,5 +39,6 @@ export default {
 </script>
 
 <style lang="scss">
+
 
 </style>
