c44f09351c39f8d6a0c055b47a16a681f7d3debc
Matteo92i <matteo.iovane@gmail.com>
2021-10-08 01:18:57 +0200

    fine parte 8

--- src/components/MainPart8.vue
+++ src/components/MainPart8.vue
@@ -32,11 +32,61 @@
                 </div>
 
                 <div class="col-4 p-0">
-                    <div class="sfondo-2"></div>
+                    <div class="sfondo-2">
+                        <h1 class="h1-menu-2">hors d'oeuvres</h1>
+                        <div class="prezzo">
+                            <h3 class="h3-menu-2">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu-2">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+
+                        <div class="prezzo">
+                            <h3 class="h3-menu-2">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu-2">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+
+                        <div class="prezzo">
+                            <h3 class="h3-menu-2">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu-2">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+
+                        <div class="prezzo">
+                            <h3 class="h3-menu-2">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu-2">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    </div>
                 </div>
 
                 <div class="col-4 p-0">
-                    <div class="sfondo-1"></div>
+                    <div class="sfondo-1">
+                        <h1 class="h1-menu">hors d'oeuvres</h1>
+                        <div class="prezzo">
+                            <h3 class="h3-menu">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+
+                        <div class="prezzo">
+                            <h3 class="h3-menu">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+
+                        <div class="prezzo">
+                            <h3 class="h3-menu">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+
+                        <div class="prezzo">
+                            <h3 class="h3-menu">ahi salmon nigri</h3>
+                            <span>48$</span>
+                        </div>
+                        <p class="p-menu">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -57,13 +107,17 @@ export default {
 }
 .sfondo-2{
     background-image: url(../assets/img/misc22x.jpg);
-    background-color: #cccccc; 
+    background-color: white;
+    
     height: 700px; 
     background-position: center; 
     background-repeat: no-repeat; 
     background-size: cover;
     width: 100%;
 }
+
+    
+
 .prezzo{
     display: flex;
     justify-content: space-between;
@@ -79,12 +133,29 @@ export default {
     padding-top: 60px;
     padding-bottom: 60px ;
 }
+.h1-menu-2{
+    color: white;
+    text-align: center;
+    padding-top: 60px;
+    padding-bottom: 60px ;
+    
+}
+
 .h3-menu{
     color: black;
+}
+.h3-menu-2{
+    color: white;
 }
     
 .p-menu{
     color: black;
+    text-align: left;
+    margin-left: 60px;
+    margin-right: 60px;
+}
+.p-menu-2{
+    color: white;
     text-align: left;
     margin-left: 60px;
     margin-right: 60px;
