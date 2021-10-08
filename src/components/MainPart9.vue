0a145194171c5711e3856e07e9664e7b01c65c7a
Matteo92i <matteo.iovane@gmail.com>
2021-10-08 08:40:18 +0200

    fine Main

--- src/components/MainPart9.vue
+++ src/components/MainPart9.vue
@@ -1,8 +1,22 @@
 <template>
     <div>
         <div class="container-fluid">
-            <div class="row">
-                <div class="banner-1"></div>
+            <div class="row mt-5">
+                <div class="banner-1">
+                    <div class="sfondo-3">
+                        <div class="prezzo">
+                            <h1 class="hr-white">braised abalone</h1>
+                            <span class="s-white">48$</span>
+                        </div>
+                        <div class="margini-1">
+                            <hr class="hr-white">
+                            <p class="p-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deserunt, quos quisquam expedita dicta, officiis blanditiis dolorem quidem culpa?</p>
+                            <button class="stile">read full article</button>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -17,14 +31,26 @@ export default {
 <style lang="scss">
 
 .banner-1{
-    background-image: url(../assets/img/page52x.jpg);
+    background-image: url(../assets/img/hero42x.jpg);
     background-color: #cccccc; 
-    height: 400px; 
+    height: 600px; 
     background-position: center; 
     background-repeat: no-repeat; 
     background-size: cover;
     width: 100%;
 }
+
+.sfondo-3{
+    height: 450px;
+    width: 400px;
+    background-color: black;
+    margin-top: 100px;
+    margin-left: 150px;
+}
+.s-white{
+    color: white;
+}
+    
     
 
 
