bd6b1515482f220639be71c7978dd6c20e3fe12e
Matteo92i <matteo.iovane@gmail.com>
2021-10-08 16:08:56 +0200

    modifiche

--- src/components/MainPart1.vue
+++ src/components/MainPart1.vue
@@ -1,37 +1,37 @@
 <template>
     <div>
-        <div class="container-fluid">
+        <div class="container">
             <div class="row mt-5">
                 <div class="col-6 p-0">
-                    <div class="col-9 mx-auto">
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum accusamus eveniet aperiam error aliquid, assumenda dolore</p>
-                    </div>
-                    <div class="col-9 mx-auto mt-5 mb-4">
-                        <h1>Food is our common ground, a universal expirience</h1>
-                    </div>
-                    <div class="col-9 mx-auto mb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ad praesentium error assumenda!
-                    </div>
-                    <div class="col-9 mx-auto">
+                    <div>
+                        <p class="p-part1"> <strong>Lorem ipsum dolor, sit amet consectetur </strong> adipisicing elit. Earum accusamus eveniet aperiam error aliquid, assumenda dolore</p>
+                        <h1 class="h1-part1">Food is our common ground, a universal expirience</h1>
+                        <p class="p-part1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ad praesentium error assumenda!</p> 
                         <img class="img-firma" src="../assets/img/signature.jpg" alt="">
                     </div>
                 </div>
 
-
                 <div class="col-6 p-0">
-                    <div class="img-part-1">
-                        <img class="center" src="../assets/img/info22x.jpg" alt="cuoco">
+                    <div class="center d-flex align-items-end justify-content-center">
+                        <h5>california rolls -- 22£</h5>
                     </div>
                 </div>
+                        
+                        
+                    
+                    
 
                     
             </div>
 
-                    
-
         </div>
     </div>
 </template>
+
+
+
+                    
+
 
 <script>
 export default {
@@ -41,21 +41,35 @@ export default {
 
 <style lang="scss">
 .center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 70%;
+  background-image: url(../assets/img/info22x.jpg);  
+    background-color: #cccccc; 
+    height: 700px; 
+    background-position: center; 
+    background-repeat: no-repeat; 
+    background-size: cover;
+    width: 100%;
+}
+.p-part1{
+    width: 400px;
+}
+.h1-part1{
+    width: 400px;
+    margin: 30px 0 ;
 }
 h1{
     text-transform:uppercase;
 }
-    
-
 .img-firma{
     width: 30%;
     height: 100px;
+    margin-top: 30px;
 }
 
+</style>
+
+
+ 
     
 
-</style>
+
+    
