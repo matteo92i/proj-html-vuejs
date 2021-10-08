ccabb81556534eb2a6c0e92d02c1035be281e256
Matteo92i <matteo.iovane@gmail.com>
2021-10-08 08:54:59 +0200

    footer inizio

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 banner-2">
+                    <div class="f-center">
+                        <img src="../assets/img/logo.png" alt="logo">
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
@@ -11,5 +20,14 @@ export default {
 </script>
 
 <style lang="scss">
+.banner-2{
+    background-image: url(../assets/img/footer2x.jpg);
+    background-color: #cccccc; 
+    height: 600px; 
+    background-position: center; 
+    background-repeat: no-repeat; 
+    background-size: cover;
+    width: 100%;
+}
 
 </style>
